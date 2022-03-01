98430213b1663db93acea70a4b3b0e84e1b892d2
zack-young-ideas <zackyoung8709@gmail.com>
2022-03-01 13:07:01 -0500

    Write end-to-end user test
    A new test was written to ensure that, on setup, the user is prompted
    to enter the database login credentials.
    
    Jest and ESLint were configured to be used for testing and quality
    assurance.

---
+++ __tests__/setup.test.js
@@ -0,0 +1,42 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+const app = require('../index');
+
+const url = 'http://localhost:3000';
+
+let browser;
+let server;
+
+beforeAll(async () => {
+  server = await app.listen(3000);
+  browser = await new Builder().forBrowser('chrome').build();
+});
+
+afterAll(async () => {
+  await browser.quit();
+  await server.close();
+});
+
+describe('Setup', () => {
+  it('should prompt user for database info', async () => {
+    // Requests made to any URL path redirect to /setup.
+    await browser.get(`${url}/`);
+    await browser.wait(until.titleIs('Setup'), 3000);
+    const currentUrl = await browser.getCurrentUrl();
+    expect(currentUrl).toEqual(`${url}/setup`);
+
+    // The user is prompted for their database login credentials.
+    const databaseNameField = await browser
+      .findElement(By.name('database-name'));
+    await databaseNameField.sendKeys('blog');
+    const usernameField = await browser.findElement(By.name('username'));
+    await usernameField.sendKeys('admin');
+    const passwordField = await browser.findElement(By.name('password'));
+    await passwordField.sendKeys('secretPassword');
+    const databaseHostField = await browser
+      .findElement(By.name('database-host'));
+    await databaseHostField.sendKeys('localhost');
+    const submitButton = await browser.findElement(By.name('submit'));
+    await submitButton.click();
+  });
+});
