35233dab86db83d5aa3aa5bde26c58d8459e900a
Zack Young <admin@youngideas.ca>
2022-03-01 12:52:18 -0500

    Create a new Express app
    Boilerplate code was written to create a new Express app and serve
    static files.

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Top-level script to initialize and configure web application object.
+ */
+
+const path = require('path');
+
+const express = require('express');
+const expressNunjucks = require('express-nunjucks');
+
+const app = express();
+const port = 3000;
+
+// Serve static files.
+app.use('/static', express.static('static'));
+
+// Configure the Nunjucks template engine.
+app.set('views', path.join(__dirname, '/templates'));
+expressNunjucks(app, { noCache: true });
+
+app.get('/', (req, res) => {
+  res.render('public/homepage');
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+  });
+} else {
+  module.exports = app;
+};
