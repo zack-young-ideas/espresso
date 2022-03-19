b4ea952b4d5006793a0cbe366965737300f0c38d
zack-young-ideas <zackyoung8709@gmail.com>
2022-03-18 21:49:45 -0400

    Write a test to ensure users can view a blog post
    A new end-to-end test was written to ensure that users can view the
    page that displays a blog post and that all the appropriate information
    is displayed.

--- tests/e2e/public.test.js
+++ tests/e2e/public.test.js
@@ -21,13 +21,17 @@ describe('Public pages', () => {
     database.getBlogPosts = () => {
       const blogPostsArray = [{
         title: 'First Post',
+        author: 'Edgar Winter',
         created: new Date(),
         body: 'This is the first post',
+        slug: 'first-post',
         views: 0,
       }, {
         title: 'Second Post',
+        author: 'Steve Miller',
         created: new Date(),
         body: 'This is the second post',
+        slug: 'second-post',
         views: 1,
       }];
       return blogPostsArray;
@@ -69,5 +73,28 @@ describe('Public pages', () => {
     const secondPostContent = await secondPost
       .findElement(By.css('p.card-text')).getText();
     expect(secondPostContent).toBe('This is the second post');
+  });
+
+  it('should display blog post content', async () => {
+    // On the homepage, if a user clicks on one of the blog posts,
+    // they are sent to the page for that blog post.
+    await browser.get(`${url}/`);
+    await browser.wait(until.titleIs('Basic Blog'), 3000);
+    const blogposts = await browser.findElements(By.className('blogpost'));
+    expect(blogposts.length).toBe(2);
+
+    const linkElement = await blogposts[0].findElement(By.css('a.text-dark'));
+    await linkElement.click();
+    await browser.wait(until.titleIs('First Post'), 3000);
+    const currentUrl = await browser.getCurrentUrl();
+    expect(currentUrl).toEqual(`${url}/blog/post/first-post`);
+    const postTitle = await browser.findElement(By.tag('h2')).getText();
+    expect(postTitle).toBe('First Post');
+    const postAuthor = await browser
+      .findElement(By.id('post-author')).getText();
+    expect(postAuthor).toBe('Edgar Winter');
+    const postContent = await browser
+      .findElement(By.className('main-content')).getText();
+    expect(postContent).toBe('This is the first post');
   });
 });
