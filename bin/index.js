e977cb2c010c2f742584205d5afe88a89b8f6973
zack-young-ideas <zackyoung8709@gmail.com>
2023-01-17 02:30:51 -0500

    Remove files output by SASS and Webpack
    The redundant CSS and JavaScript output files that are created by SASS
    and Webpack were removed, as they are redundant. The script used to
    initialize the app's configuration now uses SASS to create the app's
    stylesheets from the project's SASS files. It also uses Webpack to
    compile the front-end JavaScript files needed for the site to function
    properly. This is all done automatically when the app is installed.

--- bin/index.js
+++ bin/index.js
@@ -9,27 +9,59 @@
 const fs = require('fs');
 const path = require('path');
 
+const sass = require('sass');
+const webpack = require('webpack');
+
 const utils = require('../lib/utils');
 
 let app;
 let database;
 let settings;
 
-const configFileData = `module.exports = {
-  databaseDriver: 'mongo',
-  // WARNING: Keep secretKey and secretPepper secret!
-  secretKey: '${utils.createRandomSecret()}',
-  secretPepper: '${utils.createRandomSecret()}',
-};`;
-const filename = path.join(__dirname, '..', 'config', 'development.js');
 const port = 3000;
 
 const command = process.argv[2];
 switch (command) {
-  case 'init':
-    fs.writeFileSync(filename, configFileData);
+  case 'init': {
+    // Initilize the settings file for the website.
+    const configFileData = `module.exports = {
+      databaseDriver: 'mongo',
+      // WARNING: Keep secretKey and secretPepper secret!
+      secretKey: '${utils.createRandomSecret()}',
+      secretPepper: '${utils.createRandomSecret()}',
+    };`;
+    const configFilename = path.join(__dirname, '../config/development.js');
+    fs.writeFileSync(configFilename, configFileData);
+
+    // Compile the site's sole stylesheet from SASS files.
+    if (!fs.existsSync(path.resolve(__dirname, '../public'))) {
+      fs.mkdirSync(path.resolve(__dirname, '../public'));
+    }
+    const sassFilename = path.join(__dirname, '../public/styles.scss');
+    const sassOutput = sass.compile('./src/sass_files/main.scss');
+    fs.writeFileSync(sassFilename, sassOutput.css);
+
+    // Use Webpack to compile the site's frontend JavaScript files.
+    const webpackConfig = {
+      mode: 'development',
+      entry: {
+        deleteButton: './src/deleteButton',
+        slugAutoFill: './src/slugAutoFill',
+        tinymce: './src/tinymce',
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../public/js'),
+      },
+    };
+    webpack(webpackConfig, (err) => {
+      if (err) {
+        console.error(err.stack || err);
+      }
+    });
     break;
-  case 'run':
+  }
+  case 'run': {
     app = require('../app');
     database = require('../lib/database');
     settings = require('../config');
@@ -43,7 +75,9 @@ switch (command) {
       });
     });
     break;
-  default:
+  }
+  default: {
     console.error(`InvalidCommandError: Unrecognized command '${command}'`);
     break;
+  }
 }
