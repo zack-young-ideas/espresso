51234eaffd0eed39cf1865ae7cb607238728ebaa
Zack Young <zyoung612@gmail.com>
2024-04-09 13:55:19 -0400

    Define a write method for the Settings class
    Define a method that writes settings to the settings file on the
    local filesystem.

--- config/utils.js
+++ config/utils.js
@@ -2,11 +2,16 @@
  * Defines a class used to construct settings objects.
  */
 
+const { writeFile } = require('node:fs/promises');
+
+const utils = require('../lib/utils');
+
 class Settings {
   constructor(settingsFilename) {
+    this.filename = settingsFilename;
     let fileSettings;
     try {
-      fileSettings = require(settingsFilename);
+      fileSettings = require(settingsFilename); // eslint-disable-line
       Object.assign(this, fileSettings);
     } catch (error) {
       this.database = {
@@ -19,6 +24,22 @@ class Settings {
       this.secretKey = utils.createRandomSecret();
       this.secretPepper = utils.createRandomSecret();
     }
+  }
+
+  write() {
+    let output = 'module.exports = {\n';
+    output += '  database: {\n';
+    output += `    name: '${this.database.name}',\n`;
+    output += `    driver: '${this.database.driver}',\n`;
+    output += `    username: '${this.database.username}',\n`;
+    output += `    password: '${this.database.password}',\n`;
+    output += `    host: '${this.database.host}',\n`;
+    output += '  },\n';
+    output += '  // WARNING: Keep secretKey and secretPepper secret!\n';
+    output += `  secretKey: '${this.secretKey}',\n`;
+    output += `  secretPepper: '${this.secretPepper}',\n`;
+    output += '};';
+    return writeFile(this.filename, output, { encoding: 'utf8' });
   }
 }
 
