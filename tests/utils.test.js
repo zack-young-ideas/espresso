efe29efe6fb73d46f4528321075abce51404cde6
Zack Young <zyoung612@gmail.com>
2024-04-10 02:39:54 -0400

    Define updateDatabase method of Settings class
    Define a new method of the Settings class that updates the settings
    file with database login credentials.

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,7 @@
 const { writeFile } = require('node:fs/promises');
 path = require('node:path');
 
-const { Settings } = require('../lib/utils');
+const { getMongoConnectionUri, Settings } = require('../lib/utils');
 
 jest.mock('node:fs/promises', () => ({
   writeFile: jest.fn(() => null),
@@ -79,6 +79,50 @@ describe('Settings class', () => {
       fileContents += '};';
 
       expect(writeFile.mock.calls[0][1]).toBe(fileContents);
+    });
+  });
+
+  describe('updateDatabase method', () => {
+    it('should update database settings in settings file', () => {
+      const settings = new Settings();
+      // Mock the write method of the settings object.
+      settings.write = jest.fn(() => null);
+
+      expect(settings.write).toHaveBeenCalledTimes(0);
+
+      settings.updateDatabase({
+        databaseName: 'new_name',
+        username: 'new_user',
+        password: 'new_password',
+        databaseIp: 'new_IP_address',
+        databasePort: 'new_port',
+      });
+
+      expect(settings.database.name).toBe('new_name');
+      expect(settings.database.driver).toBe('mongo');
+      expect(settings.database.username).toBe('new_user');
+      expect(settings.database.password).toBe('new_password');
+      expect(settings.database.host).toBe('new_IP_address:new_port');
+      expect(settings.write).toHaveBeenCalledTimes(1);
+    });
+
+    it('should assign a value to the databaseUri property', () => {
+      const settings = new Settings();
+      // Mock the write method of the settings object.
+      settings.write = jest.fn(() => null);
+      const formObject = {
+        databaseName: 'blog',
+        username: 'john_smith',
+        password: 'super_secret_password',
+        databaseIp: '168.0.1.12',
+        databasePort: '27017',
+      };
+
+      expect(settings.databaseUri).toBeUndefined();
+
+      settings.updateDatabase(formObject);
+
+      expect(settings.databaseUri).toBe(getMongoConnectionUri(formObject));
     });
   });
 });
