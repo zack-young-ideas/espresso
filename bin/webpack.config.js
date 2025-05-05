19f71f01b98b5597357a8f7f399d98f3b1073a49
zack-young-ideas <zackyoung8709@gmail.com>
2025-05-04 20:51:50 -0400

    Get TinyMCE to work.
    The TinyMCE rich text editor is used by the admin site to enable
    admins to create and edit blog posts. It did not work previously
    and was not rendered properly on the page. Now, it works.

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -1,34 +1,15 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = [
   {
     mode: 'production',
     entry: {
       deleteButton: './src/deleteButton.js',
+      editor: './src/editor.js',
       slugAutoFill: './src/slugAutoFill.js',
-      tinymce: './src/editor.js',
     },
     resolve: {
       extensions: ['.js']
-    },
-    plugins: [
-      new MiniCssExtractPlugin(),
-    ],
-    module: {
-      rules: [
-        {
-          test: /skin\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        },
-        {
-          test: /content\.css$/i,
-          loader: 'css-loader',
-          options: {
-            esModule: false,
-          },
-        },
-      ],
     },
     output: {
       path: path.resolve(__dirname, '../public/js'),
